69e635415458bd7bc586a0f5506f0825c47f00b2
oleg.nikulin <oleg.nikulin@masterofcode.com>
2022-08-01 21:40:11 +0300

    coment unused code

--- Learn js.info/script.js
+++ Learn js.info/script.js
@@ -1,61 +1,84 @@
 
     let users = [];
+    let userData;
+    let userId = 0;
 
     const addUser = (evt)=>{
         evt.preventDefault();
         let registrationForm = document.querySelector('[data-registration-form]');
         let userName = document.querySelector('[data-registration-name]');
-        let userSername = document.querySelector('[data-registration-surname]');
+        let userLastName = document.querySelector('[data-registration-surname]');
         let userEmail = document.querySelector('[data-registration-email]');
-        let userAvatar = document.querySelector('[data-registration-avatar]');
 
         let user = {
+            id: userId++,
             name: userName.value,
-            sername: userSername.value,
-            email: userEmail.value,
-            avatar: userAvatar.value
+            lastName: userLastName.value,
+            email: userEmail.value
         }
         users.push(user);
         registrationForm.reset();
-        console.warn('added', {users});
-
-        // userList.textContent = JSON.stringify(users, '\t', 2)
-        var restoredSession = JSON.parse(localStorage.getItem('user'));
-        console.log(restoredSession);
+        console.warn('added', { users });
 
         localStorage.setItem('Users', JSON.stringify(users));
+        // clearData();
+        loadData();
     }
+
+    function loadData(){
+        userData = window.localStorage.getItem('Users');
+        // console.log(userData);
+        addtoPage(userData);
+    }
+
+    function clearData() {
+        let userList = document.querySelector('.user__list');
+        userList.remove();
+    }
+
+    function addtoPage(data) {
+        // const userList  = document.createElement('ul')
+        const userListWrap = document.querySelector('[data-user-list-wrapper]');
+        // console.log(userList.length);
+        // userListWrap.append(userList);
+        // userList.className = 'user__list';
+
+        userData = JSON.parse(data);
+        userData.forEach((el)=>{
+            console.log(el);
+            const userListItem  = document.createElement('li')
+            userListItem.innerHTML = '<p class="user-list-text">' + el.name + '/' + el.lastName + '</p><button class="btn">Edit</button><button class="btn" data-remove>Remove</button>';
+            userListWrap.append(userListItem);
+            userListItem.className = 'user__list-item';
+        });
+
+    }
+
+    function removeItem() {
+        let removeBtn = document.querySelector('[data-remove]');
+
+        console.log(removeBtn.parentNode.classList[0]);
+    }
+
+
 
     document.addEventListener('DOMContentLoaded', ()=>{
         let submitButton = document.querySelector('[data-registration-button]');
         submitButton.addEventListener('click', addUser);
 
-        let userList = document.querySelector('[data-user-list] p');
-        let buffer;
-
-        let userLists = users.forEach(function (index, val)) {
-            for(var i=0; i < val.length; i++){
-                var item = val[i];
-                console.log(item.name);
-                buffer+=" <li><a href='#"+item.name+"'>"+item.name+"</a></li> <li><a href='#"+item.command+"'>"+item.command+"</a></li>";
-            }
-        }
-
+        loadData()
     });
 
 
-
-// function save(){
-//     var text_to_save = registrationForm.value;
-//     localStorage.setItem("text", text_to_save); // save the item
-
-//     window.onload = function() {}
-// // }
 // class User {
-//     constructor(el) {
+//     constructor(id, name, lastName, email) {
+//         this.id = userId;
 //         this.name = userName.value;
-//         this.surname = userSername.value;
+//         this.lastName = lastName.value;
 //         this.email = userEmail.value;
-//         this.avatar = userAvatar.value;
-//     };
+//     }
+//
+//     addList() {
+//
+//     }
 // };
