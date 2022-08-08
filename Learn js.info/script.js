b8fd38d73574d377969ba1e23f2250fa614dbb12
oleg.nikulin <oleg.nikulin@masterofcode.com>
2022-08-08 22:45:19 +0300

    fixed after review

--- Learn js.info/script.js
+++ Learn js.info/script.js
@@ -1,7 +1,7 @@
 // Завдання 1
 // Створити об'єкт timer з такими полями day = Monday, hours = 12, minutes = 40, seconds = 20, finished = false
 
-let timer = {
+const timer = {
     day: 'Monday',
     hours: 12,
     minutes: 40,
@@ -23,20 +23,14 @@ function updateTimer(hours, minutes) {
 // В даній функції потрібно вивести дані таймера в такому форматі "Monday, 18:36:20 is finished false"
 
 function getTimerInfo() {
-    // let result = timer.day + ', ' + timer.hours + ':' + timer.minutes + ':' + timer.seconds + ' is finished ' + timer.finished
-    let result = `${timer.day}, ${timer.hours}:${timer.minutes}:${timer.seconds} is finished ${timer.finished}`;
-    return result;
+    return `${timer.day}, ${timer.hours}:${timer.minutes}:${timer.seconds} is finished ${timer.finished}`;
 }
 getTimerInfo()
 
 // Завдання 4
 // Створити об'єкт user з полем name = 'Oleg' в якому не можливо буде змінити поле name
 
-let user = {
-    name: 'Oleg'
-}
-
-Object.freeze(user);
+const user = Object.freeze({name: 'Oleg'});
 
 // Завдання 5
 // створити об'єкт timer з такими полями day = Monday, hours = 12, minutes = 40, seconds = 20, finished = false.
@@ -50,26 +44,25 @@ removeFinished()
 
 // Завдання 6
 // Маючи обєкт user з такими полями firstName = "Armin" lastName = "van Buuren" потрібно написати метод обєкта fullName який має повернути "Armin van Buuren"
-dj = {
+const dj = {
     firstName: 'Armin',
     lastName: 'van Buuren'
 }
 
-function Sum() {
-    this.firstName = dj.firstName,
-    this.lastName = dj.lastName,
-    this.sum = function(){
-        return `${this.firstName} ${this.lastName}`;
+const user = {
+    firstName: 'Armin',
+    lastName: 'van Buuren',
+    fullName: function () {
+        return `${this.firstName} ${this.lastName}`
     }
 }
 
-Sum()
 
 // Завдання 7
 // Потрібно створити обєкт user з такими полям firstName = "Armin" lastName = "van Buuren", а потім створити копію цього обєкту в інший обєкт user2 але так,
 // щоб при зміні полів firstName та lastName в обєктові user2 дані не змінювались в першого
 
-let djCopy = {};
+const djCopy =  { ...dj };
 
 for (let key in dj) {
     djCopy[key] = dj[key];
