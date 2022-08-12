25948c53dc0fc8a2df20939c0f5270085e75ff2e
oleg.nikulin <oleg.nikulin@masterofcode.com>
2022-08-12 23:06:24 +0300

    Uncoment first task

--- Learn js.info/script.js
+++ Learn js.info/script.js
@@ -3,76 +3,76 @@
 // У кожного з класів ProductButton та RegularButton мають бути функції які повертатимуть чи це продуктовий батон чи регуляр, також мають бути методи які повертатимуть кожне поле з common, mobile та tablet
 // Також потрібно написати функції які б оновлювали б значення в цих полів
 
-// const buttons = [
-//     { name: 'Button 1', type: 'product', common: { color: '#FFF', background_color: '#262626' }, mobile: { font_size: '16px' }, tablet: { font_size: '18px' } },
-//     { name: 'Button 2', type: 'product', common: { color: '#FFF', background_color: '#DDDDDD' }, mobile: { font_size: '16px' }, tablet: { font_size: '18px' }  },
-//     { name: 'Button 3', type: 'regular', common: { color: '#FFF', background_color: '#CDCDCD' }, mobile: { font_size: '16px' }, tablet: { font_size: '18px' }  },
-//     { name: 'Button 4', type: 'regular', common: { color: '#FFF', background_color: '#262626' }, mobile: { font_size: '16px' }, tablet: { font_size: '18px' }  },
-// ]
+const buttons = [
+    { name: 'Button 1', type: 'product', common: { color: '#FFF', background_color: '#262626' }, mobile: { font_size: '16px' }, tablet: { font_size: '18px' } },
+    { name: 'Button 2', type: 'product', common: { color: '#FFF', background_color: '#DDDDDD' }, mobile: { font_size: '16px' }, tablet: { font_size: '18px' }  },
+    { name: 'Button 3', type: 'regular', common: { color: '#FFF', background_color: '#CDCDCD' }, mobile: { font_size: '16px' }, tablet: { font_size: '18px' }  },
+    { name: 'Button 4', type: 'regular', common: { color: '#FFF', background_color: '#262626' }, mobile: { font_size: '16px' }, tablet: { font_size: '18px' }  },
+]
 
-// class Button {
-//     constructor(options) {
-//         this.name = options.name,
-//         this.type = options.type,
-//         this.common = options.common,
-//         this.mobile = options.mobile,
-//         this.tablet = options.tablet
-//     };
+class Button {
+    constructor(options) {
+        this.name = options.name,
+        this.type = options.type,
+        this.common = options.common,
+        this.mobile = options.mobile,
+        this.tablet = options.tablet
+    };
 
-//     get getType() {
-//         return this.type;
-//     }
+    get getType() {
+        return this.type;
+    }
 
-//     get getName() {
-//         return this.name;
-//     }
+    get getName() {
+        return this.name;
+    }
 
-//     get getCommon() {
-//         return this.common;
-//     }
+    get getCommon() {
+        return this.common;
+    }
 
-//     get getMobile() {
-//         return this.mobile;
-//     }
+    get getMobile() {
+        return this.mobile;
+    }
 
-//     get getTablet() {
-//         return this.tablet;
-//     }
+    get getTablet() {
+        return this.tablet;
+    }
 
-//     set setType(newType) {
-//         this.type = newType;
-//     }
+    set setType(newType) {
+        this.type = newType;
+    }
 
-//     set setName(newName) {
-//         this.name = newName;
-//     }
+    set setName(newName) {
+        this.name = newName;
+    }
 
-//     set setCommon(newCommon) {
-//         this.common = newCommon;
-//     }
+    set setCommon(newCommon) {
+        this.common = newCommon;
+    }
 
-//     set setMobile(newMobile) {
-//         this.mobile = newMobile;
-//     }
+    set setMobile(newMobile) {
+        this.mobile = newMobile;
+    }
 
-//     set setTablet(newTablet) {
-//         this.tablet = newTablet;
-//     }
-// }
+    set setTablet(newTablet) {
+        this.tablet = newTablet;
+    }
+}
 
-// class RegularButton extends Button {}
-// class ProductButton extends Button {}
+class RegularButton extends Button {}
+class ProductButton extends Button {}
 
-// let productButons;
-// let regularButons;
+let productButons;
+let regularButons;
 
-// for (let button of buttons) {
-//     if (button.type === 'product') {
-//         productButons = new ProductButton(button);
-//     } else {
-//         regularButons = new RegularButton(button);
-//     }
-// }
+for (let button of buttons) {
+    if (button.type === 'product') {
+        productButons = new ProductButton(button);
+    } else {
+        regularButons = new RegularButton(button);
+    }
+}
 
 // 2.Переписати функцію Users на клас
 
@@ -105,4 +105,3 @@ const user = new User()
 
 user.startWork();
 setTimeout(user.stopWork.bind(user), 10000);
-// setTimeout(() => {user.stopWork()}, 10000);
